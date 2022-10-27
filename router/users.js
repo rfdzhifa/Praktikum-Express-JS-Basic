b3cbdb06e920a9f6d1048dfd384eb739582ec8b8
rfdzhifa <rfdnazhifah@gmail.com>
2022-10-27 10:16:43 +0700

    modul 2

---
+++ router/users.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+
+const usercontroller = require('../controllers/user')
+
+router.get('/users', usercontroller.index)
+
+router.post('/user', usercontroller.store)
+
+router.put('/user/:id', usercontroller.update)
+
+router.delete('/user/:id', usercontroller.delete)
+
+module.exports = router
